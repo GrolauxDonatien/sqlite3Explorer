dd17b8b36941760e2e5aa5517c4cd0709838fcf6
Donatien Grolaux <dgrolaux@grayswindle.com>
2023-03-27 15:26:16 +0200

    Bugfix for display of null dates

--- editor-app/frontend/query.js
+++ editor-app/frontend/query.js
@@ -49,13 +49,13 @@ function display(results) {
             let td = $("<td>");
             switch (formats[j].type) {
                 case "datetime":
-                    td.addClass('date').text(new Date(results.rows[i][j]).toLocaleString());
+                    td.addClass('date').text(results.rows[i][j]==null?"":new Date(results.rows[i][j]).toLocaleString());
                     break;
                 case "time":
-                    td.addClass('date').text(new Date(results.rows[i][j]).toLocaleTimeString());
+                    td.addClass('date').text(results.rows[i][j]==null?"":new Date(results.rows[i][j]).toLocaleTimeString());
                     break;
                 case "date":
-                    td.addClass('date').text(new Date(results.rows[i][j]).toLocaleDateString());
+                    td.addClass('date').text(results.rows[i][j]==null?"":new Date(results.rows[i][j]).toLocaleDateString());
                     break;
                 case "number":
                     td.addClass("number"); // don't break and leak into default
