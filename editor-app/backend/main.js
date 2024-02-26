62f2d6097d229dd75467da7a5653aabbbd60b128
Donatien Grolaux <dgrolaux@grayswindle.com>
2024-02-26 08:27:00 +0100

    Update version number

--- editor-app/backend/main.js
+++ editor-app/backend/main.js
@@ -10,7 +10,7 @@ const sqlite3 = require("../../common-backend/sqlite3adapter");
 const fs = require('fs');
 const fspath = require('path');
 const LIMIT = 1000;
-const VERSION = "1.0.39";
+const VERSION = "1.0.40";
 const os = require('os');
 require('@electron/remote/main').initialize()
 
